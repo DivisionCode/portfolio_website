5b49ee6913bc8bdfa86c4499681ed2dc3d7ef2a6
Rohit Singh <singh.rsingh.rohit@gmail.com>
2025-06-30 13:38:10 +0530

    education UI , details changes done (Testing) 3006202513338

--- scripts/brief.js
+++ scripts/brief.js
@@ -1,18 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tabs = document.querySelectorAll('.tab');
     const slides = document.querySelectorAll('.slide');
+    const tables = document.querySelectorAll('.table');
 
     tabs.forEach(tab => {
         tab.addEventListener('click',() => {
             const targetId = tab.dataset.tab;
+            const type = tab.dataset.type;
 
             tabs.forEach((btn) => btn.classList.remove('active'));
             tab.classList.add('active');
 
-            slides.forEach((slide) => {
-                slide.classList.remove('active');
-            });
-            document.getElementById(targetId).classList.add('active');
+            if (type === 'slide') {
+                slides.forEach((slide) => {
+                    slide.classList.remove('active');
+                });
+                const targetSlide = document.getElementById(targetId);                
+                if (targetSlide) targetSlide.classList.add('active');
+            } else if (type === 'table') {
+                tables.forEach((table => {
+                    table.classList.remove('active');
+                }));
+                const targetSlide = document.getElementById(targetId);  
+                if (targetSlide) targetSlide.classList.add('active');
+            }
+
         });
     });
 });
